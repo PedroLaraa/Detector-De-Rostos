7b8846f3e9961bcf36260b157b11f40811aa9076
Pedro Lara <pedroalveslara@gmail.com>
2022-09-07 10:27:48 -0300

    Update DetectorComFoto.py

--- Source/DetectorComFoto.py
+++ Source/DetectorComFoto.py
@@ -6,10 +6,10 @@ face_classifier = cv2.CascadeClassifier('Model/haarcascade_frontalface_default.x
 imagem_original = cv2.imread('Img/original/testeCv2.jpg')
 copia_da_imagem_original = cv2.imread('Img/original/testeCv2.jpg')
 
-#Para que o modelo funcione corretamente é necessário mudar a imagem realizando o processamento de imagem alterando ela para escala de cinza
+# Para que o modelo funcione corretamente é necessário mudar a imagem realizando o processamento de imagem alterando ela para escala de cinza
 imagem_original_gray = cv2.cvtColor(imagem_original, cv2.COLOR_BGR2GRAY)
 
-#Variável que armazena objeto(s) com as coordenadas da face encontrada na imagem
+# Variável que armazena objeto(s) com as coordenadas da face encontrada na imagem
 faces = face_classifier.detectMultiScale(imagem_original_gray, 1.0485258, 6)
 
 if len(faces) == 0:
@@ -22,10 +22,10 @@ else:
     # Salva a imagem modificada com a face/rosto encontrado na imagem na pasta modificada    
     cv2.imwrite('Img/modificada/imagem1-modificada-com-face.jpg', face_detected)
 
-    #Comando utilizado para exibir imagem
+    # Comando utilizado para exibir imagem
     cv2.imshow('Face Detectada', face_detected)
     cv2.imshow("Imagem Original",copia_da_imagem_original)
-    #Comando utilizado para permitir que as imagems apareçam e fiquem esperando na tela até que o usuário digite o comando "Esc" para fechar as imagens depois que forem visualizadas com calma
+    # Comando utilizado para permitir que as imagems apareçam e fiquem esperando na tela até que o usuário digite o comando "Esc" para fechar as imagens depois que forem visualizadas com calma
     cv2.waitKey(0)
-    #Comando utilizado para destruir as imagens visualizadas e não manter elas na memória
+    # Comando utilizado para destruir as imagens visualizadas e não manter elas na memória
     cv2.destroyAllWindows()
